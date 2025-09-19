e68107ed3547f776f88dc73858212b2de572b92b
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2025-09-19 13:10:26 +0530

    [LineageOS] sm8350-common: Disable the usage of `thumbnail_block_model`
    * [s] word Difference
    * yaap https://github.com/yaap/frameworks_av/commit/423aeb441d17c4068941a82fd337527648f7ec8d#diff-c4447a35f24c15e7115ac8b929223e591fec9867044a2c7745bd484fdc76c33fR374
    * LineageOS https://github.com/LineageOS/android_frameworks_av/commit/5cad1681747715c3267cf42a678bf6dd1df481d4#diff-c4447a35f24c15e7115ac8b929223e591fec9867044a2c7745bd484fdc76c33fR374

--- common.mk
+++ common.mk
@@ -301,6 +301,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 $(call soong_config_set,stagefright,target_disables_thumbnail_block_model,true)
 
 # Overlays
